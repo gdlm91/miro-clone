888bfc35e474b936fabb9f22483d47fa73b5d422
Gerardo Leal <gerard.leal91@gmail.com>
2024-02-11 17:20:34 +1100

    Sidebar

---
+++ app/(dashboard)/_components/sidebar/org-list/index.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import { useOrganizationList } from "@clerk/nextjs";
+import OrgItem from "../org-item";
+
+const OrgList = () => {
+  const { userMemberships } = useOrganizationList({
+    userMemberships: {
+      infinite: true,
+    },
+  });
+
+  if (!userMemberships.data?.length) return null;
+
+  return (
+    <ul className="space-y-4">
+      {userMemberships.data.map((membership) => (
+        <OrgItem
+          key={membership.organization.id}
+          id={membership.organization.id}
+          name={membership.organization.name}
+          imageUrl={membership.organization.imageUrl}
+        />
+      ))}
+    </ul>
+  );
+};
+
+export default OrgList;
