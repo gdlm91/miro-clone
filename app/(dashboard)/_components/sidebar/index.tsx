b742cd11bdd311ea6ff2e3b1e6b0c3e3c651b43b
Gerardo Leal <gerard.leal91@gmail.com>
2024-02-11 16:24:51 +1100

    Dashboard layout

---
+++ app/(dashboard)/_components/sidebar/index.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export const Sidebar = () => {
+  return (
+    <aside className="fixed z-[1] left-0 bg-blue-950 h-full w-[60px] flex p3 flex-col gap-y-4 text-white">
+      Side
+    </aside>
+  );
+};
---
+++ app/(dashboard)/page.tsx
@@ -0,0 +1,7 @@
+import { Sidebar } from "lucide-react";
+
+const DashboardPage = () => {
+  return <div>dashboard page</div>;
+};
+
+export default DashboardPage;
